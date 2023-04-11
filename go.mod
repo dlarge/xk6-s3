1b6a3caf8869066b24a9af149fda1a777de8e3ed
Dave Largent <320517+dlarge@users.noreply.github.com>
2023-04-11 10:28:24 -0500

    Adding methods for reading byte range and generating random byte buffer; the latter is far faster in Go than JS

--- go.mod
+++ go.mod
@@ -5,26 +5,29 @@ go 1.20
 require go.k6.io/k6 v0.43.1
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.17.6
-	github.com/aws/aws-sdk-go-v2/config v1.18.18
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.17
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.30.6
+	github.com/aws/aws-sdk-go-v2 v1.17.7
+	github.com/aws/aws-sdk-go-v2/config v1.18.19
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.18
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.31.1
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
 
 require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.0 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.30 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.24 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.31 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.22 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.60
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.31 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.23 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.25 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.24 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.24 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.12.5 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.5 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.18.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.14.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.7 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/dop251/goja v0.0.0-20230128084908-78b980256d04 // indirect
--- s3.go
+++ s3.go
@@ -3,6 +3,9 @@ package s3
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -10,6 +13,7 @@ import (
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 
 	"go.k6.io/k6/js/modules"
@@ -44,6 +48,17 @@ func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
 // of the JS module.
 func (s3 *S3) Exports() modules.Exports {
 	return modules.Exports{Default: s3}
+}
+
+// Generates a random byte buffer of the given size.
+func (*S3) RandomData(size int64) []byte {
+	buf := make([]byte, size)
+	_, err := rand.Read(buf)
+	if err != nil {
+		log.Printf("Unable to generate random byte buffer: %v\n", err)
+		return nil
+	}
+	return buf
 }
 
 // Creates a new S3 client from the given configuration.
@@ -89,6 +104,32 @@ func (*S3) UploadFile(client *s3.Client, bucketName, objectKey, fileName string)
 	return err
 }
 
+// Multipart upload of the given large file to the S3 bucket with the given key.
+func (*S3) UploadLargeFile(client *s3.Client, bucketName, objectKey, fileName string, partSize int64, concurrency int) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Unable to open large file %v to upload: %v\n", fileName, err)
+	} else {
+		defer file.Close()
+		uploader := manager.NewUploader(client, func(u *manager.Uploader) {
+			u.PartSize = partSize
+			u.Concurrency = concurrency
+			u.ClientOptions = append(u.ClientOptions, s3.WithAPIOptions(
+				v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware,
+			))
+		})
+		_, err := uploader.Upload(context.Background(), &s3.PutObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(objectKey),
+			Body:   file,
+		})
+		if err != nil {
+			log.Printf("Unable to upload large file %v to %v/%v: %v\n", fileName, bucketName, objectKey, err)
+		}
+	}
+	return err
+}
+
 // Uploads the given byte data to the S3 bucket with the given key.
 func (*S3) UploadData(client *s3.Client, bucketName, objectKey string, data []byte) error {
 	_, err := client.PutObject(context.Background(),
@@ -103,4 +144,24 @@ func (*S3) UploadData(client *s3.Client, bucketName, objectKey string, data []by
 		log.Printf("Unable to upload bytes to %v/%v: %v\n", bucketName, objectKey, err)
 	}
 	return err
+}
+
+// Downloads the given byte data from the S3 bucket with the given key.
+func (*S3) DownloadDataRange(client *s3.Client, bucketName, objectKey string, begin, end int) ([]byte, error) {
+	result, err := client.GetObject(context.Background(),
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(objectKey),
+			Range:  aws.String(fmt.Sprintf("bytes=%d-%d", begin, end)),
+		})
+	if err != nil {
+		log.Printf("Unable to download bytes from %v/%v: %v\n", bucketName, objectKey, err)
+		return nil, err
+	}
+	bytes, err := io.ReadAll(result.Body)
+	if err != nil {
+		log.Printf("Unable to read bytes from %v/%v: %v\n", bucketName, objectKey, err)
+		return nil, err
+	}
+	return bytes, err
 }
